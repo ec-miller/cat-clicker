25c476d75fe5d4f17d35b6a723514f5d789691e1
Eric <ericmiller@umn.edu>
2018-07-22 16:38:26 -0500

    refactored code into model, object, and controller

--- js/main.js
+++ js/main.js
@@ -1,53 +1,64 @@
 
 
-const cats = [
-    { name: 'Bert', src: 'img/cat.jpg' },
-    { name: 'Emily', src: 'img/cat2.jpg' },
-    { name: 'Pumpkin', src: 'img/cat3.jpg' },
-    { name: 'Herbert', src: 'img/cat4.jpg' },
-    { name: 'The Twins', src: 'img/cat5.jpg' }
+const catsModel = [
+    { name: 'Bert', src: 'img/cat.jpg', count: 0 },
+    { name: 'Emily', src: 'img/cat2.jpg', count: 0 },
+    { name: 'Pumpkin', src: 'img/cat3.jpg', count: 0 },
+    { name: 'Herbert', src: 'img/cat4.jpg', count: 0 },
+    { name: 'The Twins', src: 'img/cat5.jpg', count: 0 }
 ]
 
-function createMenu() {
-    const menu = document.getElementsByClassName('menu');
-    let index = 0
-    for (cat of cats) {
+const menuView = {
+    render: function (cat,index) {
+        const menu = document.getElementsByClassName('menu');
         menu[0].innerHTML = menu[0].innerHTML + `<p id ="${index}" class="btn btn-success">${cat.name}<p>`;
-        index++
     }
 }
 
-createMenu();
+const octopus = {
+    createMenu: function() {
+        let index = 0;
+        for (cat of catsModel) {
+            menuView.render(cat,index);
+            index++;
+        }
+    },
+    createCat: function(cat) {
+        catView.loadCat(cat);
+    },
+    changeCatListener: function() {
+        const menuButtons = document.getElementsByClassName('btn');
 
-let count = 0;
-
-function loadCat(id) {
-    const card = document.getElementsByClassName('card');
-    card[0].innerHTML = `<div class="counter-container">
-					    <h3 class="cat-name">${cats[id].name}</h3>
-					    <h2 id="counter" class="counter">0</h2>
-				    </div>
-                    <img id="cat" class="cat" src="${cats[id].src}">`;
-    count = 0;
-    const catImage = document.getElementById('cat');
-    const counter = document.getElementById('counter');
-
-    catImage.addEventListener('click', function () {
-        count++;
-        counter.innerHTML = count;
-    }, false);
+        for (menuButton of menuButtons) {
+            menuButton.addEventListener('click', function () {
+                octopus.createCat(catsModel[this.id]);
+            });
+        }
+    },
+    init: function() { 
+        this.createMenu();
+        this.createCat(catsModel[0]);
+        this.changeCatListener();
+    }
 }
 
-loadCat(0);
+const catView = {
+    loadCat: function(cat) {
+        const card = document.getElementsByClassName('card');
+        card[0].innerHTML = `<div class="counter-container">
+	    				    <h3 class="cat-name">${cat.name}</h3>
+	    				    <h2 id="counter" class="counter">${cat.count}</h2>
+	    			    </div>
+                        <img id="cat" class="cat" src="${cat.src}">`;
+        const catImage = document.getElementById('cat');
+        const counter = document.getElementById('counter');
 
-
-
-
-const menuButtons = document.getElementsByClassName('btn');
-
-for (menuButton of menuButtons) {
-    menuButton.addEventListener('click', function() {
-        loadCat(this.id);
-    });
+        catImage.addEventListener('click', function () {
+            cat.count++;
+            counter.innerHTML = cat.count;
+        }, false);
+    }
 }
 
+//Get this party started
+octopus.init();
